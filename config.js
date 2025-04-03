e36b3ffc2b3f27bd0e525ba30e0d0a165a6c802d
Nguyễn Hữu Cường - TC22B <108710596+nguyenhuucuongTC21B@users.noreply.github.com>
2025-04-03 13:31:26 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// File cấu hình tối thiểu
+const config = {
+    disableModeratorIndicator: false,
+    startWithAudioMuted: false,
+    startWithVideoMuted: false,
+    enableNoisyMicDetection: false
+};
